62edc2a75431dfc9d9ee125af318feff5726b093
Chris Blow <cgblow@gmail.com>
2015-04-03 11:21:17 -0700

    various dust-ing

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 ## Bridge UI
 
-A team reference for UI production techniques and build processes.
+We're designing some bridge components in the browser.
 
-## Quick start 
+This repo keeps track of them.
 
-    `npm install -g rnpm` # install rnpm
-    `rnpm install` # to install the dependencies
-    `cd project/PROJECTNAME && gulp` # to start the compiler
+## How it works 
 
- install the packages with rnpm (`rnpm install`) [1] because some of the projects contain duplicate packages. (rnpm helps reduce the number of installed packages.)
+We're designing in the browser. To get started, install the dependencies and run the compiler.
 
-[1]: https://www.npmjs.org/package/rnpm
+In general, each component is a single template HTML file with a .dust extension.
+
+An example component is a user profile pattern. So, there's one file called `src/profile.dust` and that compiles into `www/profile.html` which you can open and see on your phone.
+
+There is an index of all of the "official" components of Bridge. 
+
+## Installing bridge-ui dependencies
+
+Most of the code we use is packaged with [NPM](https://www.npmjs.com/ "npm"). To install NPM dependencies you do this in the bridge-ui directory:
+
+                npm install
+
+This installs the tools listed in the file `package.json`.
+
+## Working locally with bridge-ui
+
+It's hosted on github pages. But, it's the local development environment that makes it easy to work with these little HTML prototypes.
+
+We use a tool called gulp. Once you've done the `npm install` step, you should be able to do something like this:
+
+                cd bridge-ui
+                gulp
+
+That will start the compiler for both the stylesheets ([Sass](sass-lang.com/)) and the HTML ([dust](http://akdubya.github.io/dustjs/ "dust") templates).
+
+While the gulp compiler is running, edit the source files in `src/` and the web-ready files in `www` will be regenerated.
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,29 @@
-// Gulp and friends
 var gulp = require('gulp');
 var rename = require("gulp-rename");
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var jshint = require('gulp-jshint');
-var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
 
-// we define the project name and version in package.js
-var pkg = require('./package.json');
-var config = require('./config/config.js');
-
-// Browser sync
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-//
-// Dust template example
-//
-// Dust templating
 var dust = require('gulp-dust');
 dust.helpers = require('dustjs-helpers').helpers;
 var dusthtml = require('gulp-dust-html');
-var dustTemplates = "src";
 
-var dustConfig = {
-  basePath: dustTemplates,
-  whitespace: true,
-  data: {
-    pkg: pkg,
-    config: config,
-    banner: '/*! ' + pkg.name + ' - v' + pkg.version + ' - ' + (new Date()).toString()
-  }
+var pkg = require('./package.json');
+var config = require('./config/config.js');
+
+var scssFiles = "src/sass/**/*.scss";
+var cssCompileDir = "www/assets/css";
+
+// Error Handling
+var ehandler = function (err) {
+  console.log('ehandler');
+  console.log(err.message);
 }
 
-// Dust templating
-gulp.task('dustify', function (cb) {
-  return gulp.src('src/*.dust').pipe(
-      dusthtml(
-        dustConfig
-      ))
-    .on('error', cb)
-    .pipe(gulp.dest('www/'));
-});
-
-// Templates
-// gulp.task('dust', function () {
-//   return gulp.src('src/dust_templates/*.js')
-//     .pipe(dust())
-//     .on('error', ehandler)
-//     .pipe(gulp.dest('src/dust_templates/compiled'));
-// });
-
-// // Nodemon
-// gulp.task('nodemon', function () {
-//   nodemon({
-//     script: 'server.js',
-//     ext: 'js json',
-//     env: {
-//       'NODE_ENV': 'development'
-//     }
-//   }).on('restart', 'lint')
-// });
-
-// Browser sync 
+// Browser-sync
+//
 var browserSyncConfig = {
   reloadDelay: 2000,
   notify: false,
@@ -71,96 +32,44 @@ var browserSyncConfig = {
   }
 }
 
-// Static server
-gulp.task('browser-sync', function () {
-  browserSync(browserSyncConfig);
-});
+// Dust templates
+var dustConfig = {
+  basePath: 'src',
+  whitespace: true,
+  data: {
+    pkg: pkg,
+    config: config,
+    banner: '/*! ' + pkg.name + ' - v' + pkg.version + ' - ' + (new Date()).toString()
+  }
+}
 
-// Reload all Browsers
-gulp.task('bs-reload', ['uglify', 'sass'], function () {
-  browserSync.reload();
-});
-
-// uglify
-gulp.task('uglify', ['dustify'], function () {
-  return gulp.src([
-      'node_modules/dustjs-linkedin/dist/dust-core.js',
-      'node_modules/dustjs-helpers/dist/dust-helpers.min.js',
-      'node_modules/crossroads/node_modules/signals/dist/signals.js',
-      'node_modules/hasher/dist/js/hasher.js',
-      'node_modules/crossroads/dist/crossroads.js',
-      'bower_components/lodash/lodash.js',
-      'node_modules/async/lib/async.js',
-      'src/dust/compiled/*.js',
-      'src/js/lib/*.js',
-      'src/js/*.js'
-    ])
-    .pipe(concat('all.js'))
-    //  .pipe(uglify())
-    .on('error', ehandler)
-    .pipe(rename(pkg.name + '.min.js'))
-    .pipe(gulp.dest('www/js/'))
-});
-// lint task for js
+// Sass stylesheets
 // 
-// gulp.task('lint', function () {
-//   return gulp.src('src/**/*.js')
-//     .pipe(jshint())
-//     .on('error', ehandler)
-// });
+var sassConfig = {
+  errLogToConsole: true,
+  includePaths: ["www/bower_components"],
+  outputStyle: "compressed"
+}
 
-// Sass task configuration
-//
-// Configure sass compiler
-var sass = require('gulp-sass');
-var scssFiles = "sass/**/*.scss";
-var cssCompileDir = "www/assets/css";
-var bowerPaths = ["www/bower_components"];
+gulp.task('dust', function (cb) {
+  return gulp.src(['src/*.dust', '!src/_*.dust']).pipe(
+      dusthtml(dustConfig))
+    .on('error', cb)
+    .pipe(gulp.dest('www/'));
+});
+
 gulp.task('sass', function () {
   return gulp.src(scssFiles)
-    .pipe(sass({
-      errLogToConsole: true,
-      includePaths: bowerPaths,
-      outputStyle: "compressed"
-    }))
+    .pipe(sass(sassConfig))
     .pipe(gulp.dest(cssCompileDir))
     .pipe(reload({
       stream: true
     }))
-    .on('error', ehandler);
 });
 
-
-// Watch HTML, js and sass
-//
-// gulp.task('watch', ['browser-sync'], function () {
-//   var watcher = gulp.watch(
-//     ['src/**/*', 'src/*', '!src/dust/compiled/*', '!.#*'], ['lint', 'dustify', 'dust', 'sass', 'uglify', 'bs-reload']
-//   );
-
-//   watcher.on('change', function (event) {
-//     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//     reload;
-//   });
-// });
-
-// Watch only sass (for performance)
-gulp.task('watch', ['sass', 'browser-sync'], function () {
-  var watcher = gulp.watch(
-    ['src/**/*', 'src/*'], ['dustify', 'sass', 'bs-reload']
-  );
-
-  watcher.on('change', function (event) {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    reload;
-  });
+// Default task
+gulp.task('default', function () {
+  gulp.watch(scssFiles, ['sass']);
+  gulp.watch('src/*.dust', ['dust']).on('change', reload);
+  browserSync(browserSyncConfig);
 });
-
-// Default task is watch everything
-gulp.task('default', ['watch']);
-
-// Error handling
-var ehandler = function (err) {
-  console.log('ehandler');
-  console.log(err.message);
-}
