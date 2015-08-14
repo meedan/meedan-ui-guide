be13e4eda54ebefb69c4ea19eac32ecdbcab6f6f
Chris Blow <cgblow@gmail.com>
2015-08-13 18:30:39 -0700

    working localization with basic En/Es capability CGB XRW

---
+++ www/meedan-web/localization.js
@@ -0,0 +1,10 @@
+var MESSAGES = {
+  'en-US': {
+    HEADER_PRIMARY: "Hello! welcome to our format.js test!",
+    HEADER_SECONDARY: "Hello! secondary"
+  },
+  'es-MX': {
+    HEADER_PRIMARY: "Hola! welcome to our format.js test!",
+    HEADER_SECONDARY: "Hola! subheader"
+  }
+};
