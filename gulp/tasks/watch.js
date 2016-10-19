66b369e0c0aec422dab31769a496e2d2db5d72b3
Chris Blow <chris@meedan.com>
2016-10-19 14:38:36 -0700

    Add Twitter and Youtube report examples

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var config = require('../config');
 
 gulp.task('watch', ['bs'], function () {
   gulp.watch(config.js.src, ['js']);
-  gulp.watch(config.misc.src, ['misc']);
+  gulp.watch(config.images.src, ['images']);
   gulp.watch(config.sass.src, ['sass']);
   gulp.watch(config.markup.src, ['markup']);
 });
