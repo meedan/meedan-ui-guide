240925e9432300e08a004efda459474e8414fd81
Chris Blow <chris@meedan.com>
2017-11-09 16:30:04 -0800

    Install React and Material-UI

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <RaisedButton label="Material UI" />
+      </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
