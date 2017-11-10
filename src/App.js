75415a0d9a91db03d2d0f4fd42d789a643efb767
Chris Blow <chris@meedan.com>
2017-11-09 16:53:22 -0800

    Add basic styles, logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import logo from './logo.svg';
+import logo from './meedan-white.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Material UI" />
-      </div>
+        <section className="header">
+          <div className="section-inner">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Meedan User Interface Guide</h2>
+          </div>
+
+          <div className="section-inner">
+            <p>
+              2017 November
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div className="section-inner">
+            <h3>Buttons</h3>
+            <RaisedButton label="Save" />
+          </div>
+        </section>
       </MuiThemeProvider>
     );
   }
